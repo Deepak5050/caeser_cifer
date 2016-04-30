969ec1c7c1237f1e01833c6d4bacf169e932e4f9
Deepak <dpoo.bang@gmail.com>
2016-04-30 17:48:43 +0530

    add a better method for cipher

--- ceaser.rb
+++ ceaser.rb
@@ -1,16 +1,26 @@
-def caesar_cipher(string, shift = 1)
-	alphabets = Array('A'..'z')
-	# creates an array of ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "[", "\\", "]", "^", "_", "`", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"] 
+def caesar_cipher(input, shift)
+	lower_case = Array('a'..'z') 
+	lower_case_shifted = lower_case.rotate(shift)
 
- 	alphabets.slice!(26, 6) #removing special/unnecesary characters
- 	alphabets_hash = Hash[alphabets.zip(alphabets.rotate(shift))]
+	upper_case = Array('A'..'Z')
+	upper_case_shifted = upper_case.rotate(shift)
 
-	string_shifted = string.chars.map { |char| alphabets_hash.fetch(char, ' ')}
-	string_shifted.join.capitalize
+	input = input.split("")
+	input_shifted = ""
+
+	input.each do |letter|
+		if lower_case.include? (letter)
+			input_shifted << lower_case_shifted[lower_case.index(letter)]
+		elsif upper_case.include? (letter)
+			input_shifted << upper_case_shifted[upper_case.index(letter)]
+		else
+			input_shifted << letter
+		end
+	end
+
+	p input_shifted
+
 end
 
-p caesar_cipher("What a string!", 5)
-
-
-
-
+caesar_cipher("What a string!", 5)
+# => "Bmfy f xywnsl!"
